68f49e770c07b62a2aa7dd70f4c0a0c88a955e28
Dylan LeBouef <dylanlebouef21@icloud.com>
2025-10-14 00:57:05 -0500

    add player.lua file

---
+++ player.lua
@@ -0,0 +1,136 @@
+player = {}
+
+function player:load()
+    -- keeps track of player position
+    self.x = 16 -- hard coded values for now..
+    self.y = 340
+    -- initial player dimensions (small mario)
+    self.width = 16
+    self.height = 16
+    -- player velocity variables for movement; 0 by default so player doesn't move by default
+    self.x_vel = 0
+    self.y_vel = 0
+    self.max_speed = 200 -- how many pixels/sec the player is able to move
+    -- to avoid player going from 0 pixels/sec to 200 pixels/sec instantly we use the two vars below:
+    self.acceleration = 4000 -- 200 / 4000 = .05 seconds to get to max speed
+    self.friction = 3500 -- 200 / 3500 = .0571 seconds to come to full stop
+    self.gravity = 1500
+    self.jump_amount = -500
+    self.grounded = false
+
+    -- table to hold players physical collision information;
+    -- player physics objects are composed of 3 parts in LOVE2D:
+    -- Body -> “where the player is and how it moves”
+    -- Shape -> “what part of the player can hit or be hit”
+    -- Fixture -> “glues the shape to the body so physics can act on it”
+    self.physics = {}
+    self.physics.body = love.physics.newBody(world, self.x, self.y, "dynamic") -- a body keeps track of position, velocity, and rotations; i.e. where an object is and where it's going
+    self.physics.body:setFixedRotation(true)
+    self.physics.shape = love.physics.newRectangleShape(self.width, self.height) -- defining the shape of the physical body. used to dictate player collisions
+    self.physics.fixture = love.physics.newFixture(self.physics.body, self.physics.shape)
+end
+
+function player:update(dt)
+    self:syncPhysics()
+    self:move(dt)
+    self:applyGravity(dt)
+    --self:applyFriction(dt)
+end
+
+function player:applyGravity(dt)
+    if not self.grounded then -- if player is not grounded, then apply gravity
+        self.y_vel = self.y_vel + self.gravity * dt
+    end
+end
+
+-- the function below just changes velocity based on keyboard input; player.physics.body is the actual object we control with keyboard input using this line in syncPhysics(): self.physics.body:setLinearVelocity(self.x_vel, self.y_vel) 
+function player:move(dt)
+    if love.keyboard.isDown('d', 'right') then
+        if self.x_vel < self.max_speed then
+            if self.x_vel + self.acceleration * dt < self.max_speed then -- ensures player body does not go over max_speed; dt is a fraction
+            self.x_vel = self.x_vel + self.acceleration * dt 
+            else
+                self.x_vel = self.max_speed
+            end
+        end
+    elseif love.keyboard.isDown('a', 'left') then
+        if self.x_vel > self.max_speed * -1 then
+            if self.x_vel - self.acceleration * dt > self.max_speed * -1 then
+            self.x_vel = self.x_vel - self.acceleration * dt
+            else
+                self.x_vel = self.max_speed * -1
+            end
+        end
+    else 
+        player:applyFriction(dt) -- else applyFriction() if no key pressed
+    end
+end
+
+function player:applyFriction(dt) -- code to stop player movement when keys are not pressed
+    if self.x_vel > 0 then -- if right velocity > 0, keep gradually reducing velocity to 0
+        if self.x_vel - self.friction * dt > 0 then
+            self.x_vel = self.x_vel - self.friction * dt
+        else
+            self.x_vel = 0
+        end
+    elseif self.x_vel < 0 then
+        if self.x_vel + self.friction * dt < 0 then -- if left velocity is < 0, keep gradually reducing velocity to 0
+            self.x_vel = self.x_vel + self.friction * dt
+        else
+            self.x_vel = 0
+        end
+    end
+end
+
+function player:syncPhysics()
+    self.x, self.y = self.physics.body:getPosition() -- syncs the physical body's position to the player's x and y coord position
+    self.physics.body:setLinearVelocity(self.x_vel, self.y_vel) 
+    -- the line above makes the physical body actually move. it uses our player's velocity variables; i.e. this will affect the body's movement based on user keyboard input, collisions, etc
+end
+
+-- beginContact() and endContact() handle fixture collisions that the player is apart of
+function player:beginContact(a, b, collision)
+    -- collision:getNormal() returns a normal vector from the collision(Contact) object
+    -- A normal vector is a unit vector perpendicular to a given object at a particular point.
+    -- when two fixtures collide, the callback fn beginContact() is called and passed a, b, and collision, where a, b are the colliding fixtures and collision is a Contact object containing information on the collision
+    -- the normal vector indicates how fixture A contacted fixture B
+    -- fixtures A and B collide:
+        -- if A -> B (A collides with B from the left), then normal vector returned = (1,0); A contacted B from the left
+        -- if B <- A, then normal vector = (-1,0); A contacted B from the right
+        -- if A collides with B from above (A falls onto B) then normal vector = (0,1); A contacted B from above
+        -- if A collides with B from below (B collides into A from below) then normal vector = (0,-1); B contacted A from below
+        -- whether a fixture is determined to be fixture A or B can seem random, so we check in the code below to see which one of A,B is the player fixture
+        -- the two if-statements do the same thing; it is just checking if the player is landing on a fixture from above; if so, set self.y_vel = 0 and set self.grounded = true to stop the constant falling of the fixture
+    
+    if self.grounded then return end -- if player already grounded, skip code below
+    local nx, ny = collision:getNormal()
+    if a == self.physics.fixture then -- if the player is the 'A' fixture
+        if ny > 0 then -- A (player) collided with B (ground)
+            self:land()
+        end
+    elseif b == self.physics.fixture then
+        if ny < 0 then -- B (player) collided with A (ground)
+            self:land()
+        end
+    end
+end
+
+function player:land()
+    self.y_vel = 0 -- player is no longer falling; fixes issue where player couldn't move because of constant increasing of y_vel
+    self.grounded = true
+end
+
+function player:jump(key)
+    if (key == 'w' or key == 'up') and self.grounded then
+        self.y_vel = self.jump_amount
+        self.grounded = false
+    end
+end
+
+function player:endContact(a, b, collision)
+    self.grounded = false
+end
+
+function player:draw()
+    love.graphics.rectangle('fill', self.x - self.width / 2, self.y - self.height / 2, self.width, self.height)
+end
